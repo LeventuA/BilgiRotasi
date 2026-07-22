42c21d31095e2355f31ad5b2b8a1f0f8ea4e6010
LeventuA <levent.ua@gmail.com>
2026-07-22 05:56:41 +0000

    Uc bin soruluk dengeli soru bankasi

--- lib/main.dart
+++ lib/main.dart
@@ -1405,7 +1405,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 const SizedBox(height: 18),
                 const Text(
-                  'Bilgi Rotası • Sürüm 1.18',
+                  'Bilgi Rotası • Sürüm 1.19',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     color: Color(0x99FFFFFF),
