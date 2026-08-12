d9581774775e0e25bba71ee3bb68c4879c228ec8
ZMilaStudio <levent.ua@gmail.com>
2026-08-12 22:54:39 +0300

    fix: enable rewarded support in closed-test profile

--- lib/ad_monetization.dart
+++ lib/ad_monetization.dart
@@ -509,6 +509,13 @@ bool supportRewardAvailabilityAfterAttempt({
   return !rewardGranted && canClaimAgain;
 }
 
+bool supportRewardEnabledForProfile({
+  required bool firebaseProductionEnabled,
+  required bool isClosedTest,
+}) {
+  return !firebaseProductionEnabled || isClosedTest;
+}
+
 class AdMonetizationDialogs {
   const AdMonetizationDialogs._();
 
@@ -667,6 +674,11 @@ class _SupportRewardCardState extends State<SupportRewardCard> {
   bool _available = false;
   bool _busy = false;
 
+  bool get _rewardProfileEnabled => supportRewardEnabledForProfile(
+    firebaseProductionEnabled: FirebaseRuntimePolicy.productionEnabled,
+    isClosedTest: AdMobConfig.isClosedTest,
+  );
+
   @override
   void initState() {
     super.initState();
@@ -676,8 +688,7 @@ class _SupportRewardCardState extends State<SupportRewardCard> {
 
   Future<void> _refresh() async {
     final available =
-        !FirebaseRuntimePolicy.productionEnabled &&
-        await _limiter.canClaim(widget.gameId);
+        _rewardProfileEnabled && await _limiter.canClaim(widget.gameId);
     if (!mounted) return;
     setState(() {
       _available = available;
@@ -686,7 +697,7 @@ class _SupportRewardCardState extends State<SupportRewardCard> {
   }
 
   Future<void> _watch() async {
-    if (_busy || !_available || FirebaseRuntimePolicy.productionEnabled) {
+    if (_busy || !_available || !_rewardProfileEnabled) {
       return;
     }
     setState(() => _busy = true);
@@ -761,7 +772,7 @@ class _SupportRewardCardState extends State<SupportRewardCard> {
           Text(
             _available
                 ? 'İsteğe bağlı reklamı tamamlayarak +10 XP kazan.'
-                : FirebaseRuntimePolicy.productionEnabled
+                : !_rewardProfileEnabled
                 ? 'Sunucu doğrulaması tamamlanana kadar +10 XP ödülü kapalı.'
                 : 'Bu oyun için +10 XP ödülü zaten alındı.',
             textAlign: TextAlign.center,
